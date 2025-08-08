b705ef6ada07c277c88122f2f5658ad36b820922
takataka06 <reyreyrey0110@gmail.com>
2025-08-08 17:07:43 +0900

    Add markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@ import "./App.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="todoを入力" />
       <button>追加</button>
       </div>
-      <div></div>
-      <div></div>
-    </>
+      <div className="incomplete-area">
+        <p className="title">未完了のtodo</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>Todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のtodo</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>Todoです</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </> 
   );
 }
